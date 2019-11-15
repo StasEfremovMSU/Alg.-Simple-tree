626aea77e831bd15dbf85f9ca3dc387ba6587cdf
StasEfremovMSU <sv.efremov@physics.msu.ru>
2019-11-15 11:14:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple tree
+ Full decription
---
+++ node.h
@@ -0,0 +1,29 @@
+#ifndef _NODE_H
+#define _NODE_H
+#include <iostream>
+using namespace std;
+template <class NODETYPE> class TreeNode
+{
+        friend class Tree<NODETYPE>;
+    public:
+        TreeNode(const NODETYPE &);
+        NODETYPE get_data();
+    protected:
+        TreeNode* left;           /* указатель на левого ребенка */
+        TreeNode* right;          /* указатель на правого ребенка */
+        TreeNode* parent;         /* указатель на родителя */
+        NODETYPE data;            /* ключ */
+};
+
+template<class NODETYPE>
+TreeNode <NODETYPE>::TreeNode(const NODETYPE &a)
+{
+        data=a;
+        left=right=0;
+}
+template <class NODETYPE>
+NODETYPE TreeNode <NODETYPE>::get_data()
+{
+        return data;
+}
+#endif
